ace888c84b7a617be430e2d2caf49ed7191643e5
CindoddCindy <cindodcindy@gmail.com>
2020-07-06 17:49:50 +0700

    location

--- app/src/main/java/cindodcindy/sirihpinang/myapplication/MainActivity.java
+++ app/src/main/java/cindodcindy/sirihpinang/myapplication/MainActivity.java
@@ -27,70 +27,113 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
 
-    Button getLocationBtn;
-    TextView locationText;
+public class MainActivity extends AppCompatActivity  {
 
-    LocationManager locationManager;
+    private FusedLocationProviderClient fusedLocationClient;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 2;
+    private LocationAddressResultReceiver addressResultReceiver;
+    private TextView currentAddTv;
+    private Location currentLocation;
+    private LocationCallback locationCallback;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getLocationBtn = (Button)findViewById(R.id.getLocationBtn);
-        locationText = (TextView)findViewById(R.id.locationText);
 
-        getLocationBtn.setOnClickListener(new View.OnClickListener() {
+        addressResultReceiver = new LocationAddressResultReceiver(new Handler());
+        currentAddTv = findViewById(R.id.textView);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        locationCallback = new LocationCallback() {
             @Override
-            public void onClick(View v) {
-                getLocation();
+            public void onLocationResult(LocationResult locationResult) {
+                currentLocation = locationResult.getLocations().get(0);
+                getAddress();
             }
-        });
+        };
+        startLocationUpdates();
 
     }
 
 
-    @Override
-    public void onLocationChanged(Location location) {
-
-        locationText.setText("Current Location: " + location.getLatitude() + ", " + location.getLongitude());
-
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        Toast.makeText(MainActivity.this, "Please Enable GPS and Internet", Toast.LENGTH_SHORT).show();
-
-    }
-
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
-
-    }
-
-    void getLocation() {
-        try {
-            ActivityCompat.requestPermissions(this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-
-            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 5, this);
+    @SuppressWarnings("MissingPermission")
+    private void startLocationUpdates() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) !=
+                PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new
+                            String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    LOCATION_PERMISSION_REQUEST_CODE);
         }
-        catch(SecurityException e) {
-            e.printStackTrace();
+        else {
+            LocationRequest locationRequest = new LocationRequest();
+            locationRequest.setInterval(2000);
+            locationRequest.setFastestInterval(1000);
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null);
         }
     }
-}
+    @SuppressWarnings("MissingPermission")
+    private void getAddress() {
+        if (!Geocoder.isPresent()) {
+            Toast.makeText(MainActivity.this, "Can't find current address, ",
+                    Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Intent intent = new Intent(this, GetAddressIntentService.class);
+        intent.putExtra("add_receiver", addressResultReceiver);
+        intent.putExtra("add_location", currentLocation);
+        startService(intent);
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull
+            int[] grantResults) {
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startLocationUpdates();
+            }
+            else {
+                Toast.makeText(this, "Location permission not granted, " + "restart the app if you want the feature", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private class LocationAddressResultReceiver extends ResultReceiver {
+        LocationAddressResultReceiver(Handler handler) {
+            super(handler);
+        }
+        @Override
+        protected void onReceiveResult(int resultCode, Bundle resultData) {
+            if (resultCode == 0) {
+                Log.d("Address", "Location null retrying");
+                getAddress();
+            }
+            if (resultCode == 1) {
+                Toast.makeText(MainActivity.this, "Address not found, ", Toast.LENGTH_SHORT).show();
+            }
+            String currentAdd = resultData.getString("address_result");
+            showResults(currentAdd);
+        }
+    }
+    private void showResults(String currentAdd) {
+        currentAddTv.setText(currentAdd);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        fusedLocationClient.removeLocationUpdates(locationCallback);
+    }
+
+
+   }
